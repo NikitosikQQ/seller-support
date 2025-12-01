b4bf1a64aeee33e29a129cd82aedd2c664106e8d
NikitosikQQ <akmn00@mail.ru>
2025-12-01 22:32:04 +0300

    добавил отдельную обработку отмены заказа для озона

--- src/main/java/ru/seller_support/assignment/domain/callback/ozon/OzonMessageTypes.java
+++ src/main/java/ru/seller_support/assignment/domain/callback/ozon/OzonMessageTypes.java
@@ -7,4 +7,5 @@ public class OzonMessageTypes {
 
     public static final String TYPE_PING = "TYPE_PING";
     public static final String TYPE_STATE_CHANGED = "TYPE_STATE_CHANGED";
+    public static final String TYPE_POSTING_CANCELLED = "TYPE_POSTING_CANCELLED";
 }
---
+++ src/main/java/ru/seller_support/assignment/service/callback/ozon/OzonPostingCancelledCallbackHandler.java
@@ -0,0 +1,51 @@
+package ru.seller_support.assignment.service.callback.ozon;
+
+import jakarta.validation.ValidationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import ru.seller_support.assignment.controller.dto.request.callback.ozon.OzonCallbackRequest;
+import ru.seller_support.assignment.controller.dto.request.callback.ozon.OzonCallbackResponse;
+import ru.seller_support.assignment.domain.callback.ozon.OzonHandleableCallbackStatuses;
+import ru.seller_support.assignment.domain.callback.ozon.OzonMessageTypes;
+import ru.seller_support.assignment.domain.enums.Marketplace;
+import ru.seller_support.assignment.service.order.OrderService;
+import ru.seller_support.assignment.service.order.OrderStatusHandler;
+
+@Service
+@RequiredArgsConstructor
+public class OzonPostingCancelledCallbackHandler extends BaseOzonCallbackHandler {
+
+    private static final String AUTHOR_OF_CHANGE = "Ozon";
+
+    private final OrderService orderService;
+    private final OrderStatusHandler orderStatusHandler;
+
+    @Override
+    public String getType() {
+        return OzonMessageTypes.TYPE_POSTING_CANCELLED;
+    }
+
+    @Override
+    public OzonCallbackResponse handle(OzonCallbackRequest request) {
+        var orderNumber = request.getPostingNumber();
+        var newRequestStatus = request.getNewState();
+        if (!StringUtils.hasText(orderNumber) || !StringUtils.hasText(newRequestStatus)) {
+            throw new ValidationException("Не указаны обязательные параметры запроса - номер отправления или его статус");
+        }
+
+        if (!OzonHandleableCallbackStatuses.HANDLEABLE_STATUSES.contains(newRequestStatus)) {
+            return OZON_BASE_OK_RESPONSE;
+        }
+
+        var order = orderService.findByNumber(orderNumber);
+        if (order == null || order.getMarketplace() != Marketplace.OZON) {
+            return OZON_BASE_OK_RESPONSE;
+        }
+
+        var newOrderStatus = OzonHandleableCallbackStatuses.OZON_STATUS_ORDER_STATUS_MAP.get(newRequestStatus);
+        orderStatusHandler.updateStatusAndSave(order, newOrderStatus, AUTHOR_OF_CHANGE);
+
+        return OZON_BASE_OK_RESPONSE;
+    }
+}
